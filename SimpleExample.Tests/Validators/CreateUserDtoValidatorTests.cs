8c69adea642e4dba222fbff7454344db69897a42
Teo Raateoja <teo.raateoja@gmail.com>
2026-02-10 12:38:32 +0200

    "Test failing tests in CI"

--- SimpleExample.Tests/Validators/CreateUserDtoValidatorTests.cs
+++
@@ -1,9 +0,0 @@
-[Fact]
-public void Should_Have_Error_When_FirstName_Is_Empty()
-{
-    CreateUserDto dto = new CreateUserDto { FirstName = "", LastName = "Meikäläinen", Email = "test@test.com" };
-    var result = _validator.TestValidate(dto);
-
-    // VÄÄRÄ ODOTUS - testi epäonnistuu!
-    result.ShouldNotHaveAnyValidationErrors();
-}
